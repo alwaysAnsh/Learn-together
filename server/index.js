c9a6087525eb064f2bfee5693487440b4c489e82
alwaysansh <anshj9y@gmail.com>
2025-11-22 16:07:49 +0530

    deploy 1

--- server/index.js
+++ server/index.js
@@ -275,4 +275,6 @@ app.delete("/api/tasks/:id", authenticateToken, async (req, res) => {
 /* -------------------- Start Server -------------------- */
 
 const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT || 5000, "0.0.0.0");
+
