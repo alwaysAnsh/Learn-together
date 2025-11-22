dd3d690ed705e76c2f433ce942bfda2e8804f75a
alwaysansh <anshj9y@gmail.com>
2025-11-22 16:05:13 +0530

    fixed thr cors origin url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "https://ansh-delta.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
